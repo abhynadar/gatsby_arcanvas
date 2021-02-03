87598403838c90c92724d2e417edffc188ec4a2c
Abhy Nadar <vabhynadar@gmail.com>
2021-02-03 22:09:36 +0530

    added google analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -59,6 +59,17 @@ module.exports = {
         tailwind: true,
         purgeOnly: [`src/assets/styles/global.css`]
       }
-    }
+    },
+    {
+      resolve: `gatsby-plugin-google-gtag`,
+      options: {
+        // You can add multiple tracking ids and a pageview event will be fired for all of them.
+        trackingIds: [
+          "G-5TNSDXYEKX", //"GA-TRACKING_ID", // Google Analytics / GA
+          "", //"AW-CONVERSION_ID", // Google Ads / Adwords / AW
+          "", //"DC-FLOODIGHT_ID", // Marketing Platform advertising products (Display & Video 360, Search Ads 360, and Campaign Manager)
+        ],
+      },
+    },
   ]
 };
