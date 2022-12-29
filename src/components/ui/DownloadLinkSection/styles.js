e9889e089d65aece4562b51175a7efc9b621a640
abhynadar <vabhynadar@yahoo.com>
2022-12-29 12:33:47 +0530

    color change
    changed color pink to blue

--- src/components/ui/DownloadLinkSection/styles.js
+++ src/components/ui/DownloadLinkSection/styles.js
@@ -8,9 +8,9 @@ export const LinkButtonSection = styled.div`
 export const LinkButton = styled.a`
 outline: none !important;
 
-${tw`py-2 px-8 rounded-full border border-pink-500 text-white`};
+${tw`py-2 px-8 rounded-full border border-blue-500 text-white`};
 
-${({ primary }) => (primary ? tw`bg-pink-500` : tw`text-pink-500`)};
+${({ primary }) => (primary ? tw`bg-blue-500` : tw`text-blue-500`)};
 
 /* This renders the buttons above... Edit me! */
 
